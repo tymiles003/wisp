82866bdb9e263f040151d16ef60baf4d30125b10
Fatima <fatima.nadine@gmail.com>
2015-01-20 20:42:41 -0800

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Wisp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.initialize_on_precompile = false
-    # config.force_ssl = true
+
+    config.force_ssl = true
   end
 end
