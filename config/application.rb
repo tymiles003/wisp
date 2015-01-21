8e7c5a1c60255a99e3d09d1ee77ebe85a0cee7cd
Fatima <fatima.nadine@gmail.com>
2015-01-20 22:29:55 -0800

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
- config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    is_www = proc { |rack_env| rack_env['SERVER_NAME'].start_with?('www') }
-    r301(/.*/, 'https://wisp-net.org$&', if: is_www)
- end
+ 
 
 module Wisp
   class Application < Rails::Application
@@ -29,6 +26,9 @@ module Wisp
     # config.i18n.default_locale = :de
     # config.assets.initialize_on_precompile = false
 
-    config.force_ssl = true
+    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    is_www = proc { |rack_env| rack_env['SERVER_NAME'].start_with?('www') }
+    r301(/.*/, 'https://wisp-net.org$&', if: is_www)
+ end
   end
 end
