e17ea6321fd07e8e74658f1f82297a9eb91172cf
Fatima <fatima.nadine@gmail.com>
2015-01-20 23:02:13 -0800

    redirect delete

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,6 @@ Rails.application.routes.draw do
 
 
 
-def redirect_subdomain
-  if request.host == 'www.wisp-net.org'
-    redirect_to 'https://wisp-net.org' + request.fullpath
-  end
-end
-
-before_filter :redirect_subdomain
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
