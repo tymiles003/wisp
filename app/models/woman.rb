c7b08fd156c40b705152727d5729cfdf0d4a6e7d
Fatima <fatima.nadine@gmail.com>
2015-02-26 23:52:07 -0800

    adding s3 buckets

--- app/models/woman.rb
+++ app/models/woman.rb
@@ -1,7 +1,9 @@
 class Woman < ActiveRecord::Base
 
 belongs_to :user      
-has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } 
+has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+:storage => :s3,
+:bucket  => ENV['MY_BUCKET_NAME'] 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	validates :image, presence: true
