90a9b085624ad1c2a528eb20c28f2f4377bd51a9
Fatima <fatima.nadine@gmail.com>
2015-03-14 22:53:05 -0700

    active admin reg user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,15 +4,16 @@ ActiveAdmin.register User do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  permit_params [:email, :password, :password_confirmation] 
 
-
+  form do |f|
+      f.inputs "User" do
+        f.input :email
+        f.input :password
+        f.input :password_confirmation
+      end
+      f.actions
+    end
 end
+
+
