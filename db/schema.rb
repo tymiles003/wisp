8ad26b08a7a2ebaa9f40d353e838d7c25e1cca81
Fatima <fatima.nadine@gmail.com>
2015-05-10 23:59:14 -0700

    date time added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331023824) do
+ActiveRecord::Schema.define(version: 20150511065124) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150331023824) do
     t.string   "tickets"
     t.text     "speakers"
     t.text     "info"
+    t.datetime "starts_at"
   end
 
   create_table "jobs", force: true do |t|
